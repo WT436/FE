31db420621f454d9a4cdd67cbf1b55cee77e9e36
WT436 <wt436.developer@gmail.com>
2020-05-31 15:52:34 +0700

    update zoom using jquery zoom

---
+++ Mảnh ghép css/ImgZoom/ImgZoom.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    $("#exzoom").exzoom({
+  
+      // thumbnail nav options
+      "navWidth": 60,
+      "navHeight": 60,
+      "navItemNum": 5,
+      "navItemMargin": 7,
+      "navBorder": 1,
+  
+      // autoplay
+      "autoPlay": true,
+  
+      // autoplay interval in milliseconds
+      "autoPlayTimeout": 2000
+      
+    });
+  
+  });
