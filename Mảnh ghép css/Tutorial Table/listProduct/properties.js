72b255e1c2f79282d0372c1e15f2a306d0ba24ee
WT436 <wt436.developer@gmail.com>
2020-09-13 18:37:48 +0700

    css

---
+++ Mảnh ghép css/Tutorial Table/listProduct/properties.js
@@ -0,0 +1,31 @@
+function GiaThiTruong() {
+    if (document.getElementById('checkdataAll').checked == true) {
+        document.querySelector('.checkdata').checked = true;
+    } else {
+        document.querySelector('.checkdata').checked = false;
+    }
+}
+
+function Gia() {
+    if (document.getElementById('checkdataAll').checked == true) {
+        document.querySelector('.checkdata').checked = true;
+    } else {
+        document.querySelector('.checkdata').checked = false;
+    }
+}
+
+function giamGiaNgay() {
+    if (document.getElementById('checkdataAll').checked == true) {
+        document.querySelector('.checkdata').checked = true;
+    } else {
+        document.querySelector('.checkdata').checked = false;
+    }
+}
+
+function soluong() {
+    if (document.getElementById('checkdataAll').checked == true) {
+        document.querySelector('.checkdata').checked = true;
+    } else {
+        document.querySelector('.checkdata').checked = false;
+    }
+}
